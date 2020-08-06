c1242da7b5c4050ab01c8208a9522774471ef70d
yiyong.lxy <yiyong.lxy@autonavi.com>
2020-08-06 17:41:49 +0800

    first commit

---
+++ precache-manifest.f41d3e7292654e8cda5d4d57c4d30f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd17397c90f4b1cfbc9ec6eee2ea7738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7e6d81632a83019f4b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f322bac943f60d490cc",
+    "url": "/static/js/2.140d28c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.140d28c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7e6d81632a83019f4b",
+    "url": "/static/js/main.5b84218a.chunk.js"
+  },
+  {
+    "revision": "204f1884735d2a064884",
+    "url": "/static/js/runtime-main.ca7a2584.js"
+  }
+]);
